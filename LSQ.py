5c10e33852b35fc614d605cb164c9b619336d995
jiyoonkm <ddjiyun@gmail.com>
2024-10-02 15:10:31 +0900

    Update LSQ.py

--- LSQ.py
+++ LSQ.py
@@ -23,9 +23,9 @@ class LsqWeight(nn.Module):
     def __init__(self, bit, per_channel):
         super().__init__()
         self.wbit = bit
-        self.thd_neg = -2**(bit-1)                                  # [-8, +7]
+        self.thd_neg = -2**(bit-1)
         self.thd_pos = 2**(bit-1)-1
-        self.per_channel = per_channel                              # whether col-wise or not
+        self.per_channel = per_channel
         self.sf = nn.Parameter(torch.ones(1))
 
     def init_from(self, x, *args, **kwargs):
@@ -40,7 +40,7 @@ class LsqWeight(nn.Module):
 
     def forward(self, x):
         # s_grad_scale = 1.0 / ((self.thd_pos * x.numel()) ** 0.5)                        # LSQ paper v1
-        s_grad_scale = 1e-4                                                             # LSQ paper v2: 1e-2 ~ 1e-4 / tried 1e-5
+        s_grad_scale = 1e-4                                                             # 1e-5 ~ 1e-2 depending on granularity
         s_scale = grad_scale(self.sf, s_grad_scale)
 
         w_q_int = torch.clamp(round_pass(x / s_scale), self.thd_neg, self.thd_pos) + 2**(self.wbit-1)
@@ -54,19 +54,14 @@ class LsqPsum(nn.Module):
         super().__init__()
         self.psbit = bit
         self.num_sigma = num_sigma
-        self.thd_neg = -2**(bit-1)                                  # [-4, +3]
+        self.thd_neg = -2**(bit-1)
         self.thd_pos = 2**(bit-1)-1
-        self.per_channel = per_channel                              # whether col-wise or not
+        self.per_channel = per_channel
         self.oc = oc
 
         num_levels = 2**bit-1
-        # # array-wise
-        # if self.per_channel:
-        #     self.sf = nn.Parameter(torch.ones(1, self.oc, 1, 1)*4/num_levels)
-        # else:
-        #     self.sf = nn.Parameter(torch.tensor([4/num_levels]))
 
-        # layer-wise
+        # 1/num_levels ~ 1
         if self.per_channel:
             self.sf = nn.Parameter(torch.ones(1, self.oc, 1, 1))
         else:
@@ -74,7 +69,7 @@ class LsqPsum(nn.Module):
 
     def forward(self, x):
         # s_grad_scale = 1.0 / ((self.thd_pos * x.numel()) ** 0.5)                        # LSQ paper v1
-        s_grad_scale = 1e-1                                                             # LSQ paper v2: activation
+        s_grad_scale = 1e-1                                                             # 1e-2 ~ 1 depending on granularity
         s_scale = grad_scale(self.sf, s_grad_scale)
         
         if self.per_channel:
@@ -88,7 +83,7 @@ class LsqPsum(nn.Module):
 
         center = torch.round(torch.mean(x_q_int))
 
-        if self.psbit == 1.5:
+        if self.psbit == 1.5: # symmetric quantizer
             # center = center + 0.5
             x_q_int = torch.where(x_q_int > center+0.5, (center+1).clone().detach(),
                       torch.where(x_q_int < center-0.5, (center-1).clone().detach(),
@@ -99,41 +94,41 @@ class LsqPsum(nn.Module):
             x_q_int = torch.clamp(x_q_int, thd_neg, thd_pos)
 
         x_q = x_q_int * s_scale
-        return x_q#, x_q_int, s_scale
+        return x_q
 
 
-class LsqPsum_int(nn.Module):
-    def __init__(self, bit, num_sigma, per_channel, oc):
-        super().__init__()
-        self.psbit = bit
-        self.num_sigma = num_sigma
-        self.thd_neg = -2**(bit-1)                                  # [-4, +3]
-        self.thd_pos = 2**(bit-1)-1
-        self.per_channel = per_channel                              # whether col-wise or not
-        self.oc = oc
+# # return integer output
+# class LsqPsum_int(nn.Module):
+#     def __init__(self, bit, num_sigma, per_channel, oc):
+#         super().__init__()
+#         self.psbit = bit
+#         self.num_sigma = num_sigma
+#         self.thd_neg = -2**(bit-1)                                  # [-4, +3]
+#         self.thd_pos = 2**(bit-1)-1
+#         self.per_channel = per_channel                              # whether col-wise or not
+#         self.oc = oc
 
-        num_levels = 2**bit-1
-        if self.per_channel:
-            self.sf = nn.Parameter(torch.ones(1, self.oc, 1, 1)*100/num_levels)
-        else:
-            self.sf = nn.Parameter(torch.tensor([100/num_levels]))
+#         num_levels = 2**bit-1
+#         if self.per_channel:
+#             self.sf = nn.Parameter(torch.ones(1, self.oc, 1, 1)*100/num_levels)
+#         else:
+#             self.sf = nn.Parameter(torch.tensor([100/num_levels]))
 
-    def forward(self, x):
-        # s_grad_scale = 1.0 / ((self.thd_pos * x.numel()) ** 0.5)                        # LSQ paper v1
-        s_grad_scale = 1e-1                                                             # LSQ paper v2: activation 1e-1
-        s_scale = grad_scale(self.sf, s_grad_scale)
-        if self.per_channel:
-            mu = torch.mean(x, dim=(0, 2, 3), keepdim=True)
-            sigma = torch.std(x, dim=(0, 2, 3), keepdim=True)
-        else:
-            mu = torch.mean(x)
-            sigma = torch.std(x)
+#     def forward(self, x):
+#         # s_grad_scale = 1.0 / ((self.thd_pos * x.numel()) ** 0.5)                        # LSQ paper v1
+#         s_grad_scale = 1e-1                                                             # LSQ paper v2: activation 1e-1
+#         s_scale = grad_scale(self.sf, s_grad_scale)
+#         if self.per_channel:
+#             mu = torch.mean(x, dim=(0, 2, 3), keepdim=True)
+#             sigma = torch.std(x, dim=(0, 2, 3), keepdim=True)
+#         else:
+#             mu = torch.mean(x)
+#             sigma = torch.std(x)
 
-        x_clip = torch.clamp(x, mu - self.num_sigma * sigma, mu + self.num_sigma * sigma)   # clip by num_sigma
+#         x_clip = torch.clamp(x, mu - self.num_sigma * sigma, mu + self.num_sigma * sigma)   # clip by num_sigma
 
-        # x_q_int = round_pass(x_clip/s_scale)
-        x_q_int = torch.clamp(round_pass(x_clip / s_scale), self.thd_neg, self.thd_pos)
-        x_q = x_q_int * s_scale
+#         # x_q_int = round_pass(x_clip/s_scale)
+#         x_q_int = torch.clamp(round_pass(x_clip / s_scale), self.thd_neg, self.thd_pos)
+#         x_q = x_q_int * s_scale
 
-        # return x_q#, x_q_int, s_scale
-        return x_q_int
+#         return x_q_int
